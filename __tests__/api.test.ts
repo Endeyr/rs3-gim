767ebe8d8b6ce053108c07d205a105c84f1bbfbc
Endeyr <aaron.thompson42594@gmail.com>
2024-10-04 14:10:51 -0400

    add comment

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -13,6 +13,7 @@ describe('API', () => {
 			body: mockPlayerResponseBody,
 		}
 
+		// player obj from await getPlayer()
 		const mockParsedPlayer = mockParsedPlayerData
 
 		// Mock axios response
