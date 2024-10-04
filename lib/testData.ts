d94a702bd14a6282270f41e14dfdd62ac120160b
Endeyr <aaron.thompson42594@gmail.com>
2024-10-04 14:06:47 -0400

    add test data based on api response

---
+++ lib/testData.ts
@@ -0,0 +1,127 @@
+export const mockPlayerResponseBody = `111986,2910,506979661
+244774,99,13115227
+241085,99,13418382
+244593,99,13258810
+179845,99,26990101
+192904,99,14645712
+175831,99,13716257
+198853,99,14976117
+236627,99,13069816
+211449,99,13247528
+215446,99,13103930
+176543,99,14104439
+92724,99,18611536
+154057,99,13948227
+214705,99,13240556
+221453,99,13144244
+141040,105,24057687
+163446,99,13818026
+178085,99,13290086
+143977,104,23520861
+140503,102,18141951
+157897,99,13214346
+173078,99,13232341
+145692,99,13950533
+190211,99,13212433
+146560,101,16991371
+179857,99,13129855
+143903,110,56630752
+115481,106,28384779
+94039,104,22813758
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+-1,-1
+75389,12555
+-1,-1
+154634,2
+-1,-1
+-1,-1
+-1,-1`
+
+export const mockParsedPlayerData = {
+	activities: {
+		bounty_hunters: { rank: -1, count: -1 },
+		bh_rogues: { rank: -1, count: -1 },
+		dominion_tower: { rank: -1, count: -1 },
+		the_crucible: { rank: -1, count: -1 },
+		castle_wars_games: { rank: -1, count: -1 },
+		ba_attackers: { rank: -1, count: -1 },
+		ba_defenders: { rank: -1, count: -1 },
+		ba_collectors: { rank: -1, count: -1 },
+		ba_healers: { rank: -1, count: -1 },
+		duel_tournament: { rank: -1, count: -1 },
+		mobilising_armies: { rank: -1, count: -1 },
+		conquest: { rank: -1, count: -1 },
+		fist_of_guthix: { rank: -1, count: -1 },
+		gg_resource_race: { rank: -1, count: -1 },
+		gg_athletics: { rank: -1, count: -1 },
+		we2_armadyl_lifetime_contribution: { rank: -1, count: -1 },
+		we2_bandos_lifetime_contribution: { rank: -1, count: -1 },
+		we2_armadyl_pvp_kills: { rank: -1, count: -1 },
+		we2_bandos_pvp_kills: { rank: -1, count: -1 },
+		heist_guard_level: { rank: -1, count: -1 },
+		heist_robber_level: { rank: -1, count: -1 },
+		cfp_5_game_average: { rank: -1, count: -1 },
+		af15_cow_tipping: { rank: -1, count: -1 },
+		af15_rats_killed_after_the_miniquest: { rank: -1, count: -1 },
+		runescore: { rank: 75389, count: 12555 },
+		clue_scrolls_easy: { rank: -1, count: -1 },
+		clue_scrolls_medium: { rank: 154634, count: 2 },
+		clue_scrolls_hard: { rank: -1, count: -1 },
+		clue_scrolls_elite: { rank: -1, count: -1 },
+		clue_scrolls_master: { rank: -1, count: -1 },
+	},
+	skills: {
+		overall: { rank: 111986, level: 2910, experience: 506979661 },
+		attack: { rank: 244774, level: 99, experience: 13115227 },
+		defence: { rank: 241085, level: 99, experience: 13418382 },
+		strength: { rank: 244593, level: 99, experience: 13258810 },
+		constitution: { rank: 179845, level: 99, experience: 26990101 },
+		ranged: { rank: 192904, level: 99, experience: 14645712 },
+		prayer: { rank: 175831, level: 99, experience: 13716257 },
+		magic: { rank: 198853, level: 99, experience: 14976117 },
+		cooking: { rank: 236627, level: 99, experience: 13069816 },
+		woodcutting: { rank: 211449, level: 99, experience: 13247528 },
+		fletching: { rank: 215446, level: 99, experience: 13103930 },
+		fishing: { rank: 176543, level: 99, experience: 14104439 },
+		firemaking: { rank: 92724, level: 99, experience: 18611536 },
+		crafting: { rank: 154057, level: 99, experience: 13948227 },
+		smithing: { rank: 214705, level: 99, experience: 13240556 },
+		mining: { rank: 221453, level: 99, experience: 13144244 },
+		herblore: { rank: 141040, level: 105, experience: 24057687 },
+		agility: { rank: 163446, level: 99, experience: 13818026 },
+		thieving: { rank: 178085, level: 99, experience: 13290086 },
+		slayer: { rank: 143977, level: 104, experience: 23520861 },
+		farming: { rank: 140503, level: 102, experience: 18141951 },
+		runecrafting: { rank: 157897, level: 99, experience: 13214346 },
+		hunter: { rank: 173078, level: 99, experience: 13232341 },
+		construction: { rank: 145692, level: 99, experience: 13950533 },
+		summoning: { rank: 190211, level: 99, experience: 13212433 },
+		dungeoneering: { rank: 146560, level: 101, experience: 16991371 },
+		divination: { rank: 179857, level: 99, experience: 13129855 },
+		invention: { rank: 143903, level: 110, experience: 56630752 },
+		archaeology: { rank: 115481, level: 106, experience: 28384779 },
+		necromancy: { rank: 94039, level: 104, experience: 22813758 },
+	},
+}
