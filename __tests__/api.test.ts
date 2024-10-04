d71454261ae8d0d60f7dbcf13304568730a7ea3e
Endeyr <aaron.thompson42594@gmail.com>
2024-10-04 14:06:57 -0400

    base api test

---
+++ __tests__/api.test.ts
@@ -0,0 +1,30 @@
+import { getPlayer } from '@/lib/api'
+import axios from 'axios'
+import { mockParsedPlayerData, mockPlayerResponseBody } from '../lib/testData'
+
+jest.mock('axios')
+
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('API', () => {
+	it('getsPlayer by name', async () => {
+		// Mocked API response
+		const mockPlayerData = {
+			body: mockPlayerResponseBody,
+		}
+
+		const mockParsedPlayer = mockParsedPlayerData
+
+		// Mock axios response
+		mockedAxios.get.mockResolvedValueOnce({ data: mockPlayerData })
+
+		// Call the function
+		const player = await getPlayer('player1')
+
+		// Assertions
+		expect(player).toEqual(mockParsedPlayer)
+		expect(mockedAxios.get).toHaveBeenCalledWith(
+			expect.stringContaining('player1')
+		)
+	})
+})
