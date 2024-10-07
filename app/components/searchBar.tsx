b8c9a438eba8fcca5fbe70e8d1bb0728d22a7dfb
Endeyr <aaron.thompson42594@gmail.com>
2024-10-07 12:22:26 -0400

    rm refactored code + update todo

--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react'
 import SearchBarForm from './searchBarForm'
 import XpTable from './xpTable'
 
-// TODO refactor, test page and components
+// TODO test page and components, add timestamps to playerData, update based on timestamps, track old vs new xp gains
 const SearchBar = () => {
 	const [playerData, setPlayerData] = useState<PlayerDataI | null>(null)
 	const [username, setUsername] = useState('')
@@ -16,37 +16,6 @@ const SearchBar = () => {
 			setPlayerData(JSON.parse(savedPlayerData) as PlayerDataI)
 		}
 	}, [])
-
-	// const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-	// 	setUserInput(e.target.value)
-	// }
-
-	// const searchName = async (e: React.FormEvent<HTMLFormElement>) => {
-	// 	e.preventDefault()
-	// 	if (userInput.trim() !== '') {
-	// 		setIsLoading(true)
-	// 		setError('')
-	// 		try {
-	// 			setName(userInput)
-	// 			const gamemode = 'normal'
-	// 			const response = await axios(
-	// 				`/api?username=${userInput}&gamemode=${gamemode}`
-	// 			)
-	// 			if (response.status === 200) {
-	// 				setPlayerData(response.data)
-	// 				localStorage.setItem('playerData', JSON.stringify(response.data))
-	// 			} else {
-	// 				throw new Error(`Failed to fetch data, status: ${response.status}`)
-	// 			}
-	// 		} catch (error) {
-	// 			setError('Error fetching player data.')
-	// 			console.error(error)
-	// 		} finally {
-	// 			setIsLoading(false)
-	// 			setUserInput('')
-	// 		}
-	// 	}
-	// }
 
 	return (
 		<>
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SearchBar from './components/searchBar'
 
 export default function Home() {
 	return (
-		<div>
+		<>
 			<SearchBar />
-		</div>
+		</>
 	)
 }
