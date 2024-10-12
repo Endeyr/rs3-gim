f880188a55f651febcfa592e4516c969bac3391f
Endeyr <aaron.thompson42594@gmail.com>
2024-10-12 16:26:01 -0400

    fix: updated user page to show xpTable

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,9 +1,33 @@
+'use client'
+
+import XpTable from '@/app/components/xpTable'
+import { PlayerContext } from '@/app/context/playerContext'
+import { PlayerContextI } from '@/types/context'
+import { useContext } from 'react'
+
 interface ProfilePagePropsI {
 	params: { username: string }
 }
 
+// TODO display chart + historic xp gains
 const ProfilePage = ({ params }: ProfilePagePropsI) => {
 	const { username } = params
-	return <div>profilePage: {username}</div>
+	const { playerDataArray, isLoading } = useContext(
+		PlayerContext
+	) as PlayerContextI
+
+	const userData = playerDataArray.find(
+		(player) => player.username === username
+	)
+
+	if (isLoading) return <div>Loading...</div>
+
+	if (!userData) return <div>PlayerData not found</div>
+
+	return (
+		<>
+			<XpTable playerData={userData} />
+		</>
+	)
 }
 export default ProfilePage
