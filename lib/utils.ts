2736b93ab5254f41b295ef4794eb82a8c517b6c8
Endeyr <aaron.thompson42594@gmail.com>
2024-10-17 16:32:25 -0400

    feat: function to check time difference on monthly xp gain

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
-import { PlayerDataI } from '@/types/playerData'
+import type { PlayerDataI } from '@/types/playerData'
+import type { MonthlyXpGainI } from '@/types/xpData'
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { MAX_AGE } from './const'
@@ -14,5 +15,13 @@ export const capitalizeFirstLetter = (str: string) => {
 export const isPlayerOutOfDate = (playerData: PlayerDataI): boolean => {
 	const timeDifference = Date.now() - new Date(playerData.timestamp).getTime()
 
+	return timeDifference > MAX_AGE
+}
+
+export const isSkillOutOfDate = (
+	existingSkillData: MonthlyXpGainI
+): boolean => {
+	const timeDifference =
+		Date.now() - new Date(existingSkillData.timestamp).getTime()
 	return timeDifference > MAX_AGE
 }
