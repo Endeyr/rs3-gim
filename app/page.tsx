b7eab44ea60181f59fdb9a89c15576bf9941d2c8
Endeyr <aaron.thompson42594@gmail.com>
2024-10-22 12:11:43 -0400

    fix: changed dashboard to hero component for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Dashboard from './components/dashboard'
+import Hero from './components/home/hero'
 
 export default function Home() {
 	return (
 		<>
-			<Dashboard />
+			<Hero />
 		</>
 	)
 }
