907d8ac7d03af7bb40cb055f1278b06ca4ba075b
Endeyr <aaron.thompson42594@gmail.com>
2024-10-22 12:11:54 -0400

    feat: add link to dashboard on navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link'
 const Navbar = () => {
 	return (
 		<nav className="min-h-[10dvh]">
-			<ul>
+			<ul className="flex gap-2">
 				<li>
 					<Link href={'/'}>Home</Link>
+				</li>
+				<li>
+					<Link href={'/dashboard'}>Dashboard</Link>
 				</li>
 			</ul>
 		</nav>
