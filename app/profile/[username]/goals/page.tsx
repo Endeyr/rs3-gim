251f96caa69708306c4f2e8d08fe186788595a86
Endeyr <aaron.thompson42594@gmail.com>
2024-10-24 08:49:19 -0400

    feat: goals page init

---
+++ app/profile/[username]/goals/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@/components/layout/container';
+
+const GoalsPage = () => {
+  return (
+    <Container>
+      <h2>Goals Page</h2>
+    </Container>
+  );
+};
+export default GoalsPage;
