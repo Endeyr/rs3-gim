17935d9a6f54936775b7182c389d160176f16af0
Endeyr <aaron.thompson42594@gmail.com>
2024-10-24 08:49:27 -0400

    fix: add s to quests

--- app/profile/[username]/quests/page.tsx
+++ app/profile/[username]/quests/page.tsx
@@ -3,7 +3,7 @@ import Container from '@/components/layout/container';
 const QuestsPage = () => {
   return (
     <Container>
-      <h2>Quest Page</h2>
+      <h2>Quests Page</h2>
     </Container>
   );
 };
