831fe62dd2b18ccab8aad974e29e510b6b1d669d
Endeyr <aaron.thompson42594@gmail.com>
2024-10-24 11:01:27 -0400

    feat: add scroll bar to select skills and all toggle

--- app/components/xpChartCheckbox.tsx
+++ app/components/xpChartCheckbox.tsx
@@ -8,9 +8,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { runemetrics } from '@/lib/const';
 import { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Checked = DropdownMenuCheckboxItemProps['checked'];
 
@@ -23,7 +24,8 @@ const XpChartCheckbox = ({
 }: {
   setSelectedSkills: (skills: string[]) => void;
 }) => {
-  const [checkedSkills, setCheckedSkills] = useState<SkillCheckedI>();
+  const [checkedSkills, setCheckedSkills] = useState<SkillCheckedI>({});
+  const [allChecked, setAllChecked] = useState(false);
   const skillNames = Object.values(runemetrics.skills);
   const handleCheckedChange = (skill: string, isChecked: Checked) => {
     const updatedCheckedSkills = {
@@ -37,24 +39,53 @@ const XpChartCheckbox = ({
     setSelectedSkills(selectedSkills);
   };
 
+  const handleToggleAllChecked = (isChecked: Checked) => {
+    const updatedCheckedSkills = skillNames.reduce((acc, skill) => {
+      acc[skill] = isChecked;
+      return acc;
+    }, {} as SkillCheckedI);
+
+    setCheckedSkills(updatedCheckedSkills);
+    setSelectedSkills(
+      Object.keys(updatedCheckedSkills).filter(
+        (skill) => updatedCheckedSkills[skill]
+      )
+    );
+    setAllChecked(!isChecked);
+  };
+
+  useEffect(() => {
+    const allSkillsChecked = skillNames.every((skill) => checkedSkills[skill]);
+    setAllChecked(allSkillsChecked);
+  }, [checkedSkills, skillNames]);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline'>Select Skills</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56'>
-        <DropdownMenuLabel>Skill</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {skillNames.map((skill) => (
+        <ScrollArea className='h-[50dvh] min-h-[20dvh] w-full rounded-md border'>
+          <DropdownMenuLabel>Skill</DropdownMenuLabel>
+          <DropdownMenuSeparator />
           <DropdownMenuCheckboxItem
-            key={skill}
-            checked={checkedSkills ? checkedSkills[skill] : false}
-            onCheckedChange={(checked) => handleCheckedChange(skill, checked)}
+            checked={allChecked}
+            onCheckedChange={(checked) => handleToggleAllChecked(checked)}
             onSelect={(e) => e.preventDefault()}
           >
-            {skill}
+            All
           </DropdownMenuCheckboxItem>
-        ))}
+          {skillNames.map((skill) => (
+            <DropdownMenuCheckboxItem
+              key={skill}
+              checked={checkedSkills ? checkedSkills[skill] : false}
+              onCheckedChange={(checked) => handleCheckedChange(skill, checked)}
+              onSelect={(e) => e.preventDefault()}
+            >
+              {skill}
+            </DropdownMenuCheckboxItem>
+          ))}
+        </ScrollArea>
       </DropdownMenuContent>
     </DropdownMenu>
   );
