67d4e7eea13e1e076079a8d3e2bff6f1b9e505bf
Endeyr <aaron.thompson42594@gmail.com>
2024-10-25 14:07:04 -0400

    feat: add todo for goals page

--- app/profile/[username]/goals/page.tsx
+++ app/profile/[username]/goals/page.tsx
@@ -1,6 +1,7 @@
 import Container from '@/components/layout/container';
 
 const GoalsPage = () => {
+  // TODO add goals to context coupled to player, save goals in local storage, add sidebar for adding goals on each page?, user can add, edit, delete, check off goals, add shared goals for group? would need to use an external storage
   return (
     <Container>
       <h2>Goals Page</h2>
