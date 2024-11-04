d51ff560f1da1eec0bc8ea2b03a770f7226c0ad6
Endeyr <aaron.thompson42594@gmail.com>
2024-11-04 07:20:57 -0500

    fix: rm window reference from localStorage

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,6 +1,6 @@
 export const setItem = (key: string, value: unknown) => {
   try {
-    window.localStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
   } catch (error) {
     console.error(error);
   }
@@ -8,7 +8,7 @@ export const setItem = (key: string, value: unknown) => {
 
 export const getItem = (key: string) => {
   try {
-    const item = window.localStorage.getItem(key);
+    const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : undefined;
   } catch (error) {
     console.error(error);
